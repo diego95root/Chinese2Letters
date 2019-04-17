a49ec89d8074e98bed1bd461691fc3d518eba39d
diego95root <minidiego@ono.com>
2019-04-17 17:46:46 +0100

    Main file to run app

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "database.h"
+#include "parser.h"
+#include "UI.h"
+
+int main(int argc, char* args[]){
+
+    char sourcePath[] = "../chars3/";
+
+    startApp(sourcePath);
+
+    return 0;
+}
